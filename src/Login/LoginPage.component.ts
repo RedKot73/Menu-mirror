dcc86e26ed678f2c8ae7ee842bd76dbbaff0337e
RedKot73 <red_kot@outlook.com>
2025-09-06 19:18:48 +0300

    Login page with form and submit handler

--- src/Login/LoginPage.component.ts
+++ src/Login/LoginPage.component.ts
@@ -1,6 +1,36 @@
 import { Component } from "@angular/core";
+import { 
+    ReactiveFormsModule,
+    FormGroup, FormControl, Validators
+} from "@angular/forms";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector: 'page-login', standalone: true,
-    template: '<h2>Вхід в систему</h2><p>Сторінка "Вхід в систему" (заглушка)</p>'
-}) export class LoginPage { }
+    template: `<h2>Вхід в систему</h2>
+    <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
+        <mat-form-field>
+            <mat-label>Ім'я користувача</mat-label>
+            <input matInput formControlName="username" />
+                    </mat-form-field>
+        <br />
+        <mat-form-field>
+            <mat-label>Пароль</mat-label>
+            <input matInput formControlName="password" type="password" />
+        </mat-form-field>
+        <br />
+        <button type="submit" [disabled]="loginForm.invalid">Увійти</button>
+    </form>`,
+    imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+    styles: [``]
+})
+export class LoginPage {
+    loginForm = new FormGroup({
+        username: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required)
+    })
+    onSubmit() {
+        console.log('Login form submitted:', this.loginForm.value);
+    }
+}
