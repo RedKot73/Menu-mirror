c6c59e0d67a2a421a20d532a22bec50c0fdd9a65
RedKot73 <red_kot@outlook.com>
2025-10-09 19:25:51 +0300

    Перемещен front

---
+++ S5Server/Front/src/app/test/Test.component.ts
@@ -0,0 +1,15 @@
+import { Component, HostListener, signal,
+    computed, ElementRef, ViewChild,
+    AfterViewInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-test',
+  imports: [CommonModule],
+  styleUrl: './Test.component.scss',
+  template: `
+  `
+})
+export class TestComponent {
+
+}
