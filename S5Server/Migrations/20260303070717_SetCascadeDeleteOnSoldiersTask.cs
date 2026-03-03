11d8bebc57b20f2d9f819b894f0bbcd4a8ddc5b4
RedKot73 <red_kot@outlook.com>
2026-03-03 18:16:17 +0200

    Set cascade delete on SoldiersTask foreign key relationship
    - Updated the foreign key constraint for the 'soldiers_task' table to use cascade delete behavior when a related soldier is deleted.
    - Modified the migration file to reflect this change and ensure proper database schema alignment.

---
+++ S5Server/Migrations/20260303070717_SetCascadeDeleteOnSoldiersTask.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace S5Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetCascadeDeleteOnSoldiersTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_soldiers_task_soldiers_soldier_id",
+                schema: "docs",
+                table: "soldiers_task");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_soldiers_task_soldiers_soldier_id",
+                schema: "docs",
+                table: "soldiers_task",
+                column: "soldier_id",
+                principalSchema: "core",
+                principalTable: "soldiers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_soldiers_task_soldiers_soldier_id",
+                schema: "docs",
+                table: "soldiers_task");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_soldiers_task_soldiers_soldier_id",
+                schema: "docs",
+                table: "soldiers_task",
+                column: "soldier_id",
+                principalSchema: "core",
+                principalTable: "soldiers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
