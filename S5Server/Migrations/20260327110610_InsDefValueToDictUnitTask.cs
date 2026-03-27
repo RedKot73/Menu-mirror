fc242887e8d6377d97af2fdb8088690d5b21bcd5
red_kot <red_kot@outlook.com>
2026-03-27 13:10:34 +0200

    Add migration to insert default values into dict_area_type, dict_area, and dict_unit_task tables
    - Created a new migration that inserts default values into the dict_area_type, dict_area, and dict_unit_task tables.
    - Ensured that the inserts do not conflict with existing records by using "on conflict(id) do nothing".

---
+++ S5Server/Migrations/20260327110610_InsDefValueToDictUnitTask.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace S5Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsDefValueToDictUnitTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO dict.dict_area_type (id, value, ""comment"", short_value)
+VALUES ('00000000-0000-0000-0000-000000000002'::uuid, '---', 'Не вказано', 'Не вказано')
+on conflict(id) do nothing;
+INSERT INTO dict.dict_area (id, area_type_id, city_code_id, coords, value, ""comment"", shortvalue)
+VALUES ('00000000-0000-0000-0000-000000000002'::uuid, '00000000-0000-0000-0000-000000000002'::uuid, NULL, NULL, '---', NULL, '---')
+on conflict(id) do nothing;
+INSERT INTO dict.dict_unit_task (id, value, ""comment"", amount, with_means, area_type_id)
+VALUES ('00000000-0000-0000-0000-000000000002'::uuid, '---', NULL, 0.00, false, '00000000-0000-0000-0000-000000000002'::uuid)
+on conflict(id) do nothing;
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
