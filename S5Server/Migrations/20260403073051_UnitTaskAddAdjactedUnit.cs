ba27fbd5340cc0e84a52ae2c64756e5a51cfdab5
red_kot <red_kot@outlook.com>
2026-04-03 10:51:33 +0300

    Add adjacted unit fields to units_task table and update model snapshot
    - Introduced new columns `adjacted_short_name` (string) and `adjacted_unit_id` (Guid) in the `units_task` table.
    - Created an index on `adjacted_unit_id` for performance optimization.
    - Added foreign key constraint linking `adjacted_unit_id` to the `units` table in the `core` schema.
    - Updated the model snapshot to reflect the new properties in the `UnitTask` entity.
    - Modified launch settings to include a migration command for easier database updates.

---
+++ S5Server/Migrations/20260403073051_UnitTaskAddAdjactedUnit.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace S5Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UnitTaskAddAdjactedUnit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "adjacted_short_name",
+                schema: "docs",
+                table: "units_task",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "adjacted_unit_id",
+                schema: "docs",
+                table: "units_task",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_units_task_adjacted_unit_id",
+                schema: "docs",
+                table: "units_task",
+                column: "adjacted_unit_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_units_task_units_adjacted_unit_id",
+                schema: "docs",
+                table: "units_task",
+                column: "adjacted_unit_id",
+                principalSchema: "core",
+                principalTable: "units",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_units_task_units_adjacted_unit_id",
+                schema: "docs",
+                table: "units_task");
+
+            migrationBuilder.DropIndex(
+                name: "ix_units_task_adjacted_unit_id",
+                schema: "docs",
+                table: "units_task");
+
+            migrationBuilder.DropColumn(
+                name: "adjacted_short_name",
+                schema: "docs",
+                table: "units_task");
+
+            migrationBuilder.DropColumn(
+                name: "adjacted_unit_id",
+                schema: "docs",
+                table: "units_task");
+        }
+    }
+}
